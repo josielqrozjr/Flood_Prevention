8a0b9e28ab7bcb26e97c22456f19065cb4262e29
Queiroz Jr <josielqueirozjr@icloud.com>
2024-05-18 14:10:32 -0300

    Create ORM database
    Criação do ORM Python usando Flask-SQLAlchemy, no entanto, falta ser testado

--- Dashboard_Web/models/user/client.py
+++ Dashboard_Web/models/user/client.py
@@ -5,12 +5,10 @@ from models.db import datetime
 class Client(db.Model):
     __tablename__ = 'client'
 
-    id= db.Column('id', 
-                  db.Integer, 
-                  db.ForeignKey(Users.id), 
-                  nullable=False, 
-                  autoincrement=True, 
-                  primary_key=True)
+    id = db.Column(db.Integer, db.ForeignKey(Users.id), primary_key=True)
     
-    creation_date = db.Column(db.DateTime(), nullable = False, default=datetime.now())
-    update_date = db.Column(db.DateTime(), nullable = False, default=datetime.now())
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    update_date = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+    # Relationship
+    user = db.relationship('Users', back_populates='client', lazy=True)
--- Dashboard_Web/models/user/contact.py
+++ Dashboard_Web/models/user/contact.py
@@ -1,17 +1,19 @@
 from models import db
 from models.user.users import Users
+from models.db import datetime
 
 class Contact(db.Model):
     __tablename__ = 'contact'
 
-    id= db.Column('id', 
-                  db.Integer, 
-                  db.ForeignKey(Users.id), 
-                  nullable=False, 
-                  autoincrement=True, 
-                  primary_key=True)
+    id= db.Column(db.Integer, autoincrement=True, primary_key=True)
 
-    country_code= db.Column(db.Tinyint(2, unsigned=True, zerofill=True), nullable=False, default='55')
-    area_code= db.Column(db.Tinyint(unsigned=True, zerofill=True), nullable=False)
-    number= db.Column(db.Integer(unsigned=True), nullable=False)
-    users_id = db.Column(db.Integer, db.ForeignKey(Users.id))
+    country_code = db.Column(db.SmallInteger, nullable=False, default=55)
+    area_code = db.Column(db.SmallInteger, nullable=False)
+    number = db.Column(db.BigInteger, nullable=False)
+    update_date = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+    # Foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey(Users.id), nullable=False)
+    
+    # Relationship
+    user = db.relationship('Users', back_populates='contact')
